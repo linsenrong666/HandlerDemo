3b538561a6c9cde41372fe0ee3ae6d47e9e19316
gene.dev5 <gene.dev5@le.com>
2017-10-14 17:05:44 +0800

    static handler

--- app/src/main/java/com/linsr/handlerdemo/TwoActivity.java
+++ app/src/main/java/com/linsr/handlerdemo/TwoActivity.java
@@ -1,5 +1,7 @@
 package com.linsr.handlerdemo;
 
+import android.app.Activity;
+import android.nfc.Tag;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.HandlerThread;
@@ -7,17 +9,42 @@ import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
+import java.lang.ref.WeakReference;
+
+/**
+ * 静态内部类使用handler，防止内存泄漏
+ */
 public class TwoActivity extends AppCompatActivity {
 
-    private String TAG = "===TwoActivity";
+    private static final String TAG = "===TwoActivity";
+
+    MyHandler mMyHandler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mMyHandler = new MyHandler(this);
+        mMyHandler.sendEmptyMessage(0);
+    }
 
-        getMainLooper().getQueue();
+    private static class MyHandler extends Handler {
+        private WeakReference<Activity> mWeakReference;
 
+        public MyHandler(Activity activity) {
+            mWeakReference = new WeakReference<>(activity);
+        }
+
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            Activity activity = mWeakReference.get();
+            switch (msg.what) {
+                case 0:
+                    Log.d(TAG,"receive msg ，thread:"+Thread.currentThread().getName());
+                    break;
+            }
+        }
     }
 
 }
